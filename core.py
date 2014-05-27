391cb1de60a31aa1a164bdc8d93268bc6cb33c86
Marcus Silva <marcusviniciusns@gmail.com>
2014-05-26 23:36:08 -0230

    Some tests using python-crontab

--- core.py
+++ core.py
@@ -1,5 +1,9 @@
+from lib.crontab.crontab import CronTab
+
 def load():
-  pass
+  cron = CronTab(user=True)
+  for job in cron:
+    print str(job) + " Enabled = " + str(job.is_enabled())
 
 def save():
   pass
