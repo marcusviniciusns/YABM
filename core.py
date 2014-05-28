ba3860982354772adea6e92ff650473eababd1aa
Marcus Silva <marcusviniciusns@gmail.com>
2014-05-28 00:31:35 -0230

    Added classes to abstract the types used by YABM

--- core.py
+++ core.py
@@ -1,9 +1,50 @@
 from lib.crontab.crontab import CronTab
+import re
+
+class ConfigType:
+  """Enum-like class that represents the Configuration Type"""
+  LOCAL = 0
+  REMOTE = 1
+
+class ConfigMode:
+  """Enum-like class that represents the Configuration Mode"""
+  SIMPLE = 0
+  EXPERT = 1
+
+class ConfigSchedule:
+  """Holds the Configuration Schedule"""
+  def __init__(self):
+    self.minute = None
+    self.hour = None
+    self.dom = None
+    self.month = None
+    self.dow = None
+  
+  def parse_schedule(schedule):
+    pass
+
+class Config:
+  """Represents an YABM Configuration"""
+  def __init__(self):
+    self.id = None
+    self.name = None
+    self.type = ConfigType.LOCAL
+    self.source = None
+    self.destination = None
+    self.schedule = None
+    self.active = False
+    self.mode = ConfigMode.SIMPLE
+    self.command = None
+    self.command_options = None
+
+  def parse_metadata(metadata):
+    pass
 
 def load():
   cron = CronTab(user=True)
   for job in cron:
-    print str(job) + " Enabled = " + str(job.is_enabled())
+    if re.match("^tool=YABM", job.comment):
+      print str(job) + " Enabled = " + str(job.is_enabled())
 
 def save():
   pass
