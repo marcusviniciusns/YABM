29694abff7af0311930028e12217661b9804b9da
Marcus Silva <marcusviniciusns@gmail.com>
2014-06-06 23:47:45 -0230

    Many improvements in the GUI

--- gui.py
+++ gui.py
@@ -8,19 +8,19 @@ class MainWindow:
     self.cm = CronManager()
     self.lbConfigs = None
 
-    root = Tk()
-    root.title("YABM - Yet Another Backup Manager")
+    self.root = Tk()
+    self.root.title("YABM - Yet Another Backup Manager")
     
-    frmTop = Frame(root)
-    frmTop.grid(row=0, column=0, sticky=N+S+E+W)
-    frmBottom = Frame(root)
-    frmBottom.grid(row=1, column=0, sticky=E)
+    frmTop = Frame(self.root)
+    frmTop.grid(row=0, column=0, padx=2, pady=2, sticky=N+S+E+W)
+    frmBottom = Frame(self.root)
+    frmBottom.grid(row=1, column=0, padx=2, pady=2, sticky=E)
 
     lblSelect = Label(frmTop, text="Please select a configuration to load:")
     lblSelect.grid(row=0, column=0, sticky=W)
 
     sbConfigs = Scrollbar(frmTop, orient=VERTICAL)
-    self.lbConfigs = Listbox(frmTop, yscrollcommand=sbConfigs.set)
+    self.lbConfigs = Listbox(frmTop, yscrollcommand=sbConfigs.set, width=75)
     self.lbConfigs.config(font=("TkFixedFont"))
     self.lbConfigs.grid(row=1, column=0, sticky=N+S+E+W)
     sbConfigs.config(command=self.lbConfigs.yview)
@@ -28,29 +28,32 @@ class MainWindow:
  
     btnNew = Button(frmBottom, text="New")
     btnNew.grid(row=0, column=0)
+    btnNew.bind("<Button-1>", self.new)
     btnLoad = Button(frmBottom, text="Load")
     btnLoad.grid(row=0, column=1)
+    btnLoad.bind("<Button-1>", self.load)
 
     self.load_configs()
 
-    root.mainloop()
+    self.root.mainloop()
   
   def load_configs(self):
     self.lbConfigs.delete(0, END)
 
-    header  = self.format("Name", 12) + "|"
-    header += self.format("Active", 6) + "|"
+    header  = self.format("Name", 15) + "|"
     header += self.format("Source", 20) + "|"
-    header += self.format("Destination", 20) 
-
+    header += self.format("Destination", 20) + "|"
+    header += self.format("Type", 6) + "|"
+    header += self.format("Active", 6)
     self.lbConfigs.insert(END, header)
     
     self.cm.load_all()
     for config in self.cm.configs:
-      row  = self.format(str(config.name), 12) + "|"
-      row += self.format(str(config.enabled), 6) + "|"
+      row  = self.format(str(config.name), 15) + "|"
       row += self.format(str(config.command.source), 20) + "|"
-      row += self.format(str(config.command.destination), 20)
+      row += self.format(str(config.command.destination), 20) + "|"
+      row += self.format(str(config.type), 6) + "|"
+      row += self.format(str(config.enabled), 6)
       self.lbConfigs.insert(END, row) 
 
   def format(self, str, size):
@@ -60,8 +63,43 @@ class MainWindow:
       return str 
     return str[:size-3] + "..."
 
+  def new(self, event):
+    configWindow = ConfigWindow(self.root, None) 
 
-def test(*args):
-  print "test"
+  def load(self, event):
+    items = self.lbConfigs.curselection()
+    if len(items) == 0 or len(items) > 1 or int(items[0]) == 0:
+      dialog = InfoDialog(self.root, "Please select a valid configuration")      
+    else:
+      configWindow = ConfigWindow(self.root, self.cm.configs[int(items[0])]) 
 
+class ConfigWindow:
+  def __init__(self, parent, config):
+
+    title = ""
+    if config == None:
+      title = "New Configuration"
+    else:
+      title = config.name
+    
+    configWindow = Toplevel(master=parent)
+    configWindow.title(title)
+
+
+class InfoDialog:
+  def __init__(self, parent, info):
+    self.dialog = Toplevel(parent)
+
+    frmTop = Frame(self.dialog)
+    frmTop.grid(row=0, column=0, padx=2, pady=2, sticky=N+S+E+W)
+    frmBottom = Frame(self.dialog)
+    frmBottom.grid(row=1, column=0, padx=2, pady=2, sticky=N+S+E+W)
+
+    lblInfo = Label(self.dialog, text=info)
+    lblInfo.grid(row=0, column=0)
+    btnOk = Button(self.dialog, text="OK", command=self.ok)
+    btnOk.grid(row=1, column=0)
+
+  def ok(self):
+    self.dialog.destroy()
 
